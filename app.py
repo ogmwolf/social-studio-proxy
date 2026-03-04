2e30a8f8849369e0acf05e1bea57c4aaaad6228b
ogmwolf <wolftopia@gmail.com>
2026-03-04 15:47:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import requests
 from flask import Flask, request, Response
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/2/<path:path>', methods=['GET', 'OPTIONS'])
+@app.route('/', defaults={'path': ''}, methods=['GET', 'OPTIONS'])
+@app.route('/<path:path>', methods=['GET', 'OPTIONS'])
 def proxy(path):
-    url = f'https://api.twitter.com/2/{path}'
-    headers = {'Authorization': request.headers.get('Authorization')}
-    resp = requests.get(url, headers=headers, params=request.args)
-    return Response(resp.content, status=resp.status_code, content_type='application/json')
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    }
+    if request.method == 'OPTIONS':
+        return Response('', 200, headers)
+    
+    url = f'https://api.twitter.com/{path}'
+    auth = request.headers.get('Authorization', '')
+    resp = requests.get(url, headers={'Authorization': auth}, params=request.args)
+    headers['Content-Type'] = 'application/json'
+    return Response(resp.content, status=resp.status_code, headers=headers)
 
 if __name__ == '__main__':
     app.run()
